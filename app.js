cee5a02bbbb778988e80e9176ed414e84520b98c
leandroVR1 <yeifry.121@gmail.com>
2025-08-26 18:32:48 -0500

    Se creo un array el cual va almacenar los nombres y una funcion encargada de validar e ingresar los nombres en el array

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let arrayAmigos = [];
+
+function agregarAmigo() {
+    let amigo = document.getElementById("amigo").value;
+
+    if (amigo === "") {
+        alert("Por favor ingresa un nombre");
+        return;
+    }else{
+        arrayAmigos.push(amigo);
+        console.log(arrayAmigos);
+    }
+   
+}
